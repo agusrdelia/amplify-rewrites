9b00cc39801d3ac26b8fedbdc6748a3716565c67
Agustin Delia <agusrdelia@gmail.com>
2022-08-11 20:08:37 -0300

    Update

--- next-basic-rewrite/next.config.js
+++ next-basic-rewrite/next.config.js
@@ -3,23 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   async rewrites() {
-    return {afterFiles: [
-      {
-        source:
-          '/:path*-:type([btl]{0,1}):year(\\d{4}):month(\\d{2}):day(\\d{2})(-?):id(\\d{4}).html',
-        destination: '/news',
-      },
-      {
-        source:
-          '/:tagname-t:tagid(\\d+)',
-        destination: '/tag',
-      },
-      {
-        source:
-          '/category/:slug',
-        destination: '/seccion/:slug',
-      },
-    ]}
+    return {
+      afterFiles: [
+        {
+          source:
+            '/__public/:buildid/:path',
+          destination: '/:path',
+        },
+        {
+          source:
+            '/:path*-:type([btl]{0,1}):year(\\d{4}):month(\\d{2}):day(\\d{2})(-?):id(\\d{4}).html',
+          destination: '/news',
+        },
+        {
+          source:
+            '/:tagname-t:tagid(\\d+)',
+          destination: '/tag',
+        },
+        {
+          source:
+            '/category/:slug',
+          destination: '/seccion/:slug',
+        },
+      ]
+    }
   }
 }
 
